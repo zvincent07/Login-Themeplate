1aa7c5c9495e349de67dc0f2b9ade918690f6635
zvincent07 <vincentlaylo07@gmail.com>
2026-01-12 21:29:44 +0800

    change text

--- frontend/src/components/dashboards/layout/ProfileDropdown.jsx
+++ frontend/src/components/dashboards/layout/ProfileDropdown.jsx
@@ -234,7 +234,7 @@ const ProfileDropdown = ({ user, isOpen, onClose, sidebarOpen, profileButtonRef 
               d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
             />
           </svg>
-          Activity Logs
+          My Activity
         </button>
 
         <div className="border-t border-gray-200 dark:border-slate-700 my-0.5"></div>
